a7680dfd1bdf000fa602a5b817ff464af4fc7fab
Edward290394 <68308411+Edward290394@users.noreply.github.com>
2020-07-15 13:09:38 -0500

    Add files via upload

---
+++ Prueba-Meto/Prueba_Metodologias/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Prueba_Metodologias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sad = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sad
+            // 
+            this.sad.AutoSize = true;
+            this.sad.Location = new System.Drawing.Point(286, 119);
+            this.sad.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.sad.Name = "sad";
+            this.sad.Size = new System.Drawing.Size(24, 13);
+            this.sad.TabIndex = 0;
+            this.sad.Text = "sad";
+            this.sad.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 82);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Metodologias_A";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sad);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sad;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
